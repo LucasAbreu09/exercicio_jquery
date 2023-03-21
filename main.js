1489557e292ab98bb67c0affcbc235ef5881c65c
Lucas Abreu <lucasabreu.c18@gmail.com>
2023-03-21 17:27:04 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+        e.preventDefault()
+        const novaTarefa = $('#tarefa').val()
+
+        $(`<li>${novaTarefa}</li>`).appendTo('ul')
+
+        $('li').click(function() {
+            $(this).addClass('item_completado');
+        });
+    })
+
+})
